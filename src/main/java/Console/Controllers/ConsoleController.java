00b0d9f2aeb52f6a8cd8caed862268c7a293d11a
emilyperegrine <peregrineemily@gmail.com>
2016-08-27 21:28:10 +0100

    Loaded fxml

--- src/main/java/Console/Controllers/ConsoleController.java
+++ src/main/java/Console/Controllers/ConsoleController.java
@@ -2,10 +2,14 @@ package Console.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * Created by emilyperegrine on 27/08/2016.
  */
 public class ConsoleController {
-    @FXML private VBox Conatainer;
+    public VBox container;
+    public Text console;
+
+
 }
--- src/main/java/Console/GUIConsole.java
+++ src/main/java/Console/GUIConsole.java
@@ -3,6 +3,8 @@ package Console;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -13,19 +15,8 @@ import javafx.stage.Stage;
 public class GUIConsole extends Application implements IConsole {
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/console.fxml"));
 
         Scene scene = new Scene(root, 300, 250);
 
